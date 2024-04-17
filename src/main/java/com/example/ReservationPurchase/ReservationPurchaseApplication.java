a036563d9ea1574cebb642ea12d02d22a7389e4a
Ooyd <qk1413@naver.com>
2024-04-17 18:01:23 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/ReservationPurchase/ReservationPurchaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.ReservationPurchase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationPurchaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationPurchaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ReservationPurchase/ReservationPurchaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ReservationPurchase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationPurchaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
